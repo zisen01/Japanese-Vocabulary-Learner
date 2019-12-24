b98b11b226d4fe5e943e2a9b99155cf6a0eee667
zisen01 <zisen@ualberta.ca>
2019-12-23 18:10:14 -0700

    fix bug on 'path space regardless' functionality

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 sh compile.sh
-MY_PATH=$PWD | tr " " "\ " 
-
-echo "#!/bin/sh\njava -classpath $MY_PATH MainActivity"  > vocablearner
+MY_PATH="#!/bin/sh\njava -classpath ${PWD// /\ }"
+echo "$MY_PATH/ MainActivity"  > vocablearner
 chmod +x vocablearner
 cp vocablearner /usr/local/bin
 sudo chmod 755 /usr/local/bin/vocablearner
