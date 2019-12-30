fcfc7c0bbbc80f629f5bf2c773a91b6a9fe298db
zisen01 <zisen@ualberta.ca>
2019-12-30 08:06:11 -0700

    move default vocab lists to corresponding directory on install

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-sh compile.sh
+sh compile.sh # compile
+# create link to bin
 MY_PATH="#!/bin/sh\njava -classpath ${PWD// /\ }"
 echo "$MY_PATH/ MainActivity"  > vocablearner
 chmod +x vocablearner
 cp vocablearner /usr/local/bin
 sudo chmod 755 /usr/local/bin/vocablearner
+# move vocab lists to ~/Jvocab_learner/
+cp -R ./vocab_list $HOME/Jvocab_learner/
