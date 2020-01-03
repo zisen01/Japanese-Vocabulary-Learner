131ff741b9ae81eae002811f2c638b8d0b19a983
zisen01 <zisen@ualberta.ca>
2020-01-03 13:15:29 -0700

    fix installing bug and add uninstaller

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ echo "$MY_PATH/ MainActivity"  > vocablearner
 chmod +x vocablearner
 cp vocablearner /usr/local/bin
 sudo chmod 755 /usr/local/bin/vocablearner
-# move vocab lists to ~/Jvocab_learner/
+# move vocab lists to ~/Jvocab_learner/vocab_list/
+mkdir -p $HOME/Jvocab_learner/vocab_list/
 cp -R ./vocab_list $HOME/Jvocab_learner/
---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm /usr/local/bin/vocablearner
+rm -R $HOME/Jvocab_learner
+rm *.class
+rm vocablearner
